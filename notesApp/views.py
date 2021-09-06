d51fce7acb750fd57234193ab5d534b28d72a331
ombasa <ombasaherman@gmail.com>
2021-09-07 01:37:16 +0300

    views & rest_framework

--- notesApp/views.py
+++ notesApp/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from . import models
+from .import serializers
+
+
+class NoteList(generics.ListAPIView):
+	queryset = models.Note.objects.all()
+	serializer_class = serializers.NoteSerializer
+
+
+class NoteDetail(generics.RetrieveUpdateAPIView):
+	queryset = models.Note.objects.all()
+	serializer_class = serializers.NoteSerializer
